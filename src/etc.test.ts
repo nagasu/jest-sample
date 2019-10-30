6a5e228b080d4011923a149e9f4468b36729fcf4
Satoshi Nagasu <s-nagasu.03@nexsimo.co.jp>
2019-10-30 18:00:29 +0900

    update

--- src/etc.test.ts
+++ src/etc.test.ts
@@ -36,4 +36,17 @@ describe('etc', (): void => {
   test('but there is a "stop" in Christoph', () => {
     expect('Christoph').toMatch(/stop/);
   });
+
+  function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+  }
+
+  test('compiling android goes as expected', () => {
+    expect(compileAndroidCode).toThrow();
+    expect(compileAndroidCode).toThrow(Error);
+
+    // You can also use the exact error message or a regexp
+    expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+    expect(compileAndroidCode).toThrow(/JDK/);
+  });
 });
